9567694c0937de6bc0800390931ef9e7d1ea794a
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-15 17:59:33 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace AttributesExample
+{
+    // We tag our Custom Attribute with the AttributeUsage attribute.
+    // AttributeTargets.All - Can be applied to anything, has no limitations
+    // AllowMultiple - Can be used multiple times
+    // Inherited - Will be inherited by the derived classes of whatever class it was used on
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = true)]
+    public class MyCustomAttribute : Attribute // A custom attribute must be inherited by System.Attribute
+    {
+        // Properties that allows read/write access, theyre set by the constructor
+        public string Name { get; set; }
+        public string Action { get; set; }
+
+        // The constructor sets the fields when instantiated through the properties
+        public MyCustomAttribute(string n, string a)
+        {
+            Name = n;
+            Action = a;
+        }
+
+    }
+
+    public class Animal
+    {
+        // Notice we didnt have to write MyCustomAttribute, C# removes "Attribute" to make it easier for us.
+        [MyCustom("Accessor", "Sets / Gets the name of the animal")]
+        public string Name { get; set; }
+        [MyCustom("Accessor", "Sets / Gets the age of the animal")]
+        public int Age { get; set; }
+
+        public Animal(string n, int a)
+        {
+            Name = n;
+            Age = a;
+        }
+
+        // A simple demonstration of the attribute Obsolete
+        // string message - The message to show when attempting access to a depracated method
+        // bool error - if set to false, it will crash the compiler with an error to prevent usage
+        // if false, it'll run with a warning
+        [Obsolete("Do not use, use the new implementation instead", true)]
+        public void EatOld()
+        {
+            Console.WriteLine("The animal eats");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} eats");
+        }
+    }
+
+    public class App
+    {
+        public static void Main()
+        {
+            Animal myPet = new("Ella", 3);
+
+            Console.WriteLine(myPet.Name);
+            Console.WriteLine(myPet.Age);
+
+            myPet.Name = "Buddy";
+            Console.WriteLine(myPet.Name);
+
+            myPet.Eat();
+
+            // Throws an error if we try to use it, because error is set to true, if set to false, it will allow usage with a warning
+            // myPet.EatOld();
+        }
+    }
+
+
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+Demonstrates how attributes are used in C# with .NET.
+
+This practice project showcases:
+- The built-in `AttributeUsage`.
+- Custom attributes.
+- The `Obsolete` attribute.
